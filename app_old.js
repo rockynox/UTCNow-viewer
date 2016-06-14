3244f3b4bb8a62d82991e479241115789219c1de
Léo <leo.maisonneuve@gmail.com>
2016-06-14 11:58:43 +0200

    Add files via upload
    initial commit

---
+++ app_old.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var nib = require('nib');
+var stylus = require('stylus');
+var logger = require('morgan');
+
+var app = express()
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(logger('dev'));
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public'
+        , compile: compile
+    }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+    res.render('index',
+        {title: 'Home'}
+    )
+})
+
+
+app.listen(3000)
